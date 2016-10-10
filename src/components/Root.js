2720dfc23fdfdef925a3e2de634cc72b461fe244
z <z>
2016-10-10 23:52:47 +0300

    Fix: lesson 5 finished

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+
 import TodoApp from './TodoApp';
 
 const Root = ({ store }) => {
   return (
     <Provider store={store}>
-      <TodoApp />
+      <Router history={browserHistory}>
+        <Route path='/' component={TodoApp} />
+      </Router>
     </Provider>
   );
 };
